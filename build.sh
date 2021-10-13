b6778753f982095c781d8c0b72b8ba086ba5370b
Diego Canez <canezdiego@gmail.com>
2021-10-13 12:18:31 -0500

    Added wacom monitor script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/development/resources/dotfiles
 mkdir -p $HOME/development/resources/scripts
 mkdir -p $HOME/development/algorithms
 
-sudo pacman -S tmux yarn nvm neovim clang xclip xsel ccls ruby github-cli solaar net-tools
+sudo pacman -S tmux yarn nvm neovim clang xclip xsel ccls ruby github-cli solaar net-tools thunar
 
 gh auth login
 
@@ -31,7 +31,8 @@ config config --local status.showUntrackedFiles no
 gh repo clone dgcnz/competitive-programming $HOME/development/algorithms/competitive-programming
 gh repo clone dgcnz/cp-library $HOME/development/algorithms/cp-library
 gh repo clone atcoder/ac-library $HOME/development/algorithms/ac-library
+gh repo clone dgcnz/scripts $HOME/development/resources/scripts
 
-pamac build google-chrome dbg-macro discord zotero zoom spotify
+pamac build google-chrome dbg-macro discord zotero zoom spotify pa-applet
 sudo pacman -Rcn lightdm
-sudo pacman -S manjaro-pipewire okular
+sudo pacman -S manjaro-pipewire okular pavucontrol
